838d5870b1e203c4e80174cea8ad3122ab34e583
philips-of-ng@philips <philipsedun@gmail.com>
2024-09-18 11:47:33 +0100

    updated

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,4 +1,5 @@
 // THIS IS THE CODE THAT OPENS AND CLOSE THE MIDDLE NAV THAT WORKS ONLY ON MOBILE SCREEN
+
 const openMiddleNav = document.getElementById('middle-nav-open');
 const middleNav = document.getElementById('middle-nav');
 
@@ -15,5 +16,42 @@ openMiddleNav.addEventListener('click', () => {
     openMiddleNav.innerHTML = `<div></div><div></div><div></div><div></div>`;
   }
 });
+
+// THIS IS THE CODE THAT OPENS AND CLOSES THE INTERNET BANKING OFFCANVAS
+const ibdControl = document.getElementById('internet-banking-dsv-control');
+const ibdOffcanvas = document.getElementById('internet-banking-dsv');
+let focusOffcanvas = false;
+
+function closeOffcanvas() {
+  ibdOffcanvas.classList.add('animate__fadeOutRight');
+
+  ibdOffcanvas.addEventListener('animationend', () => {
+    ibdOffcanvas.classList.remove('ibd-open', 'animate__fadeInRight', 'animate__fadeOutRight');
+  }, { once: true });
+}
+
+ibdControl.addEventListener('mouseover', () => {
+  ibdOffcanvas.classList.remove('ibd-close', 'animate__fadeOutRight'); 
+  ibdOffcanvas.classList.add('animate__fadeInRight', 'ibd-open');
+});
+
+ibdControl.addEventListener('mouseleave', () => {
+  if (!focusOffcanvas) {
+    closeOffcanvas();
+  }
+});
+
+ibdOffcanvas.addEventListener('mouseover', () => {
+  focusOffcanvas = true;
+  ibdOffcanvas.classList.remove('animate__fadeOutRight');
+});
+
+ibdOffcanvas.addEventListener('mouseleave', () => {
+  focusOffcanvas = false;
+  closeOffcanvas();
+});
+
+
+
 
 
